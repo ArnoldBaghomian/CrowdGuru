50410dbc6700e89b49ba45b03af08025c5064cc4
Zack Winchell <zackwinchell@gmail.com>
2016-03-01 22:19:16 -0800

    moved from CDNs to bower

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Richard' });
+  res.render('index', { title: 'CrowdGuru' });
 });
 
 module.exports = router;
