2f416e81a393b3738d668704f774bc38c0771042
Zack Winchell <zackwinchell@gmail.com>
2016-03-03 11:03:03 -0800

    moved to function use of 'use strict', added missing semicolon

--- src/js/controllers/bidCtrl.js
+++ src/js/controllers/bidCtrl.js
@@ -1,8 +1,8 @@
-'use strict';
 
 // controller that will be called when splash page is loaded
 var app = angular.module('');
 
 app.controller('bidCtrl', function() {
+  "use strict";
   console.log("bidCtrl");
 });
--- src/js/main.js
+++ src/js/main.js
@@ -27,7 +27,7 @@ app.config(function($stateProvider, $urlRouterProvider) {
       url: '/profile',
       templateUrl: './partials/profilePage.html'
       // controller: 'requestCtrl'
-    })
+    });
 
   $urlRouterProvider.otherwise('/');
 });
