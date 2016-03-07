10c311f65245c92a3a7742a03f7813682b6bfbc4
MaltbyRL <RLMaltby@gmail.com>
2016-03-06 16:32:57 -0800

    getting up to date

--- src/js/controllers/request/requestViewCtrl.js
+++ src/js/controllers/request/requestViewCtrl.js
@@ -3,5 +3,4 @@ app.controller('requestViewCtrl', function($scope, $stateParams) {
   "use strict";
   console.log("requestViewCtrl");
   $scope.id = $stateParams.requestId;
-  console.log("Params: ", $stateParams.requestId)
 });
