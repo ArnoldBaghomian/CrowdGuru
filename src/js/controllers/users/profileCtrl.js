607469b51e11ae048c258467a6ea020838f07b00
MaltbyRL <RLMaltby@gmail.com>
2016-03-07 16:29:58 -0800

    Done work on profile and profile control

--- src/js/controllers/users/profileCtrl.js
+++ src/js/controllers/users/profileCtrl.js
@@ -1,9 +1,23 @@
 // controller that will be called when profile page is loaded
-app.controller("profileCtrl", function($state) {
+app.controller("profileCtrl", function($state, $scope, $rootScope) {
   "use strict";
   if(!Cookies('authToken')) {
-    Cookies("originalUrl", "profile");
+    Cookies("originalUrl", location.pathname);
     $state.go("login");
   }
+  $rootScope.Req = true;
+
+  $scope.showReq = function(){
+    console.log("Req: ", $scope.Req)
+    $scope.Guru = false
+    $scope.Req = true
+  }
+
+  $scope.showGuru = function(){
+    console.log("Guru: ", $scope.Guru)
+    $scope.Guru = true
+    $scope.Req = false
+  }
+
   console.log("profileCtrl");
 });
