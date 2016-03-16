e9e1d4f1aef444908198f2a62080b221bbaa06ff
Zack Winchell <zackwinchell@gmail.com>
2016-03-15 22:24:52 -0700

    corrected newly found linting issues

--- src/js/main.js
+++ src/js/main.js
@@ -96,17 +96,17 @@ app.config(function($provide) {
 app.run(function(){
   $(window).scroll(function(){
     if($(window).scrollTop() + 100 > $(document).height() - $(window).height() || ($(window).width() < 725)){
-      $('footer').hide();
+      $("footer").hide();
     } else{
-      $('footer').show();
+      $("footer").show();
     }
   });
 });
 
 
 app.run(function($window, $rootScope, $location) {
-  $window.ga('create', 'UA-75164642-1', 'auto');
-  $rootScope.$on('$stateChangeSuccess', function (event) {
-    $window.ga('send', 'pageview', $location.path());
+  $window.ga("create", "UA-75164642-1", "auto");
+  $rootScope.$on("$stateChangeSuccess", function (event) {
+    $window.ga("send", "pageview", $location.path());
 });
 });
