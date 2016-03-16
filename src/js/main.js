13bbde78fdfbe5f54c2bcc0cc76d63fa7b82f894
Zack Winchell <zackwinchell@gmail.com>
2016-03-16 14:06:02 -0700

    lessened DOM access of footer visibility function

--- src/js/main.js
+++ src/js/main.js
@@ -3,82 +3,82 @@ console.log("crowdGuru app");
 // used to populate the partials as well as linking them to there controllers
 app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
   $stateProvider
-    .state("splash", {
-      url: "/",
-      templateUrl: "./partials/splashPage.html",
-      data: {
-        css: ["css/splash.css"]
-      }
-    })
-    .state("bidView", {
-      url: "/bid/view/{bidId}",
-      templateUrl: "./partials/bid/view.html",
-      controller: "bidViewCtrl"
-    })
-    .state("bidNew", {
-      url: "/bid/new/{requestId}",
-      templateUrl: "./partials/bid/new.html",
-      controller: "bidNewCtrl",
-      data: {
-        css: ["css/bid/new.css"]
-      }
-    })
-    .state("login", {
-      url: "/users/login",
-      templateUrl: "./partials/users/login.html",
-      data:{
-        css: ["css/users/login.css"]
-      },
-      controller: "loginCtrl"
-    })
-    .state("profile", {
-      url: "/users/profile?userId",
-      templateUrl: "./partials/profilePage.html",
-      data:{
-        css: ["css/users/profile.css"]
-      },
-      controller: "profileCtrl"
-    })
-    .state("changePassword", {
-      url: "/users/password/change",
-      templateUrl: "./partials/users/password/change.html",
-      controller: "changePasswordCtrl",
-      data:{
-        css: ["css/users/password/change.css"]
-      }
-    })
-    .state("forgotPassword", {
-      url: "/users/password/forgot",
-      templateUrl: "./partials/users/password/forgot.html",
-      controller: "forgotPasswordCtrl"
-    })
-    .state("register", {
-      url: "/users/register",
-      templateUrl: "./partials/users/register.html",
-      data:{
-        css: ["css/users/register.css"]
-      },
-      controller: "registerCtrl"
-    })
+  .state("splash", {
+    url: "/",
+    templateUrl: "./partials/splashPage.html",
+    data: {
+      css: ["css/splash.css"]
+    }
+  })
+  .state("bidView", {
+    url: "/bid/view/{bidId}",
+    templateUrl: "./partials/bid/view.html",
+    controller: "bidViewCtrl"
+  })
+  .state("bidNew", {
+    url: "/bid/new/{requestId}",
+    templateUrl: "./partials/bid/new.html",
+    controller: "bidNewCtrl",
+    data: {
+      css: ["css/bid/new.css"]
+    }
+  })
+  .state("login", {
+    url: "/users/login",
+    templateUrl: "./partials/users/login.html",
+    data:{
+      css: ["css/users/login.css"]
+    },
+    controller: "loginCtrl"
+  })
+  .state("profile", {
+    url: "/users/profile?userId",
+    templateUrl: "./partials/profilePage.html",
+    data:{
+      css: ["css/users/profile.css"]
+    },
+    controller: "profileCtrl"
+  })
+  .state("changePassword", {
+    url: "/users/password/change",
+    templateUrl: "./partials/users/password/change.html",
+    controller: "changePasswordCtrl",
+    data:{
+      css: ["css/users/password/change.css"]
+    }
+  })
+  .state("forgotPassword", {
+    url: "/users/password/forgot",
+    templateUrl: "./partials/users/password/forgot.html",
+    controller: "forgotPasswordCtrl"
+  })
+  .state("register", {
+    url: "/users/register",
+    templateUrl: "./partials/users/register.html",
+    data:{
+      css: ["css/users/register.css"]
+    },
+    controller: "registerCtrl"
+  })
 
-    .state("requestNew", {
-      url: "/request/new",
-      templateUrl: "./partials/request/new.html",
-      controller: "requestNewCtrl"
-    })
-    .state("requestSearch", {
-      url: "/request/search",
-      templateUrl: "./partials/request/search.html",
-      data:{
-        css: ["css/request/search.css"]
-      },
-      controller: "requestSearchCtrl"
-    })
-    .state("requestView", {
-      url: "/request/view/{requestId}",
-      templateUrl: "./partials/request/view.html",
-      controller: "requestViewCtrl"
-    });
+  .state("requestNew", {
+    url: "/request/new",
+    templateUrl: "./partials/request/new.html",
+    controller: "requestNewCtrl"
+  })
+  .state("requestSearch", {
+    url: "/request/search",
+    templateUrl: "./partials/request/search.html",
+    data:{
+      css: ["css/request/search.css"]
+    },
+    controller: "requestSearchCtrl"
+  })
+  .state("requestView", {
+    url: "/request/view/{requestId}",
+    templateUrl: "./partials/request/view.html",
+    controller: "requestViewCtrl"
+  });
   $locationProvider.html5Mode(true);
   $urlRouterProvider.otherwise("/");
 });
@@ -94,11 +94,12 @@ app.config(function($provide) {
   });
 });
 app.run(function(){
-  $(window).scroll(function(){
-    if($(window).scrollTop() + 100 > $(document).height() - $(window).height() || ($(window).width() < 725)){
-      $("footer").hide();
+  let $window = $(window), $document = $(document), $footer = $("footer");
+  $window.scroll(function(){
+    if($window.scrollTop() + 100 > $document.height() - $window.height() || ($window.width() < 725)){
+      $footer.hide();
     } else{
-      $("footer").show();
+      $footer.show();
     }
   });
 });
@@ -108,5 +109,5 @@ app.run(function($window, $rootScope, $location) {
   $window.ga("create", "UA-75164642-1", "auto");
   $rootScope.$on("$stateChangeSuccess", function (event) {
     $window.ga("send", "pageview", $location.path());
-});
+  });
 });
